8147ec3710f521b7bc2266d7af1f6212ceccbd16
JDAIN <poker98@web.de>
2019-04-08 12:12:17 +0200

    speed improvements and moved line now shown in red

--- sketch.js
+++ sketch.js
@@ -2,27 +2,29 @@
 
 
 //change for more bars (rec. 4)
-const scl =4;
+const scl = 4;
 let numArray;
 let lenOfArray;
 let i;
+
 function setup() {
     //change for other canvas size
     createCanvas(800, 400);
     background("black");
-    numArray = new Array(floor(width/scl));
-    lenOfArray= numArray.length;
+    numArray = new Array(floor(width / scl));
+    lenOfArray = numArray.length;
     i = 0;
     //fill array
     for (let i = 0; i < numArray.length; i++) {
         numArray[i] = random(0, height);
     }
-    console.log(numArray.length)
+    console.log(numArray.length);
+    noLoop();
 }
 
 
 function draw() {
-
+    setTimeout(redraw, .00001); // where 10 is the minimum time between frames in ms
     background("black");
 
 
@@ -34,14 +36,16 @@ function draw() {
         numArray[i + 1] = a;
     }
     i++;
+
+    show();
     //minus the already checked, gets faster if less elements are checked
-    if(i===width/scl + (lenOfArray-(width/scl))){
-        i=0;
+    if (i === width / scl + (lenOfArray - (width / scl))) {
+        i = 0;
         lenOfArray--;
     }
-    show();
 
-    if(lenOfArray<=0){
+
+    if (lenOfArray <= 0) {
         noLoop();
         background("green");
         show();
@@ -51,11 +55,15 @@ function draw() {
 }
 
 function show() {
-    for (let i = 0; i < numArray.length; i++) {
+
+    for (let k = 0; k < numArray.length; k++) {
         strokeWeight(3.8);
         stroke(150);
         strokeCap(SQUARE);
-        line(i*scl, height, i*scl, height - numArray[i]);
+        line(k * scl, height, k * scl, height - numArray[k]);
+
     }
+    stroke('red');
+    line(i * scl, height, i * scl, height - numArray[i]);
 }
 
